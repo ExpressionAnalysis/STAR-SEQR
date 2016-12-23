a3fd7ab1f705da20a2c4c2f1373bb2a6242c2d4d
Jeff Jasper <jasper1918@gmail.com>
2016-12-23 15:13:35 -0500

    new module

---
+++ starseqr_utils/gtf_convert.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import logging
+import gzip
+
+logger = logging.getLogger("STAR-SEQR")
+
+
+# modified from https://github.com/chapmanb/cloudbiolinux/blob/master/utils/prepare_tx_gff.py
+def file_exists(fname):
+    """Check if a file exists and is non-empty."""
+    try:
+        return fname and os.path.exists(fname) and os.path.getsize(fname) > 0
+    except OSError:
+        return False
+
+
+def gtf_to_genepred(gtf, out_file):
+    if file_exists(out_file):
+        logger.warn("Skipping gtf_to_genepred as files already exist!")
+        return out_file
+    cmd = "gtfToGenePred -allErrors -ignoreGroupsWithoutExons -genePredExt -geneNameAsName2 {gtf} {out_file}"
+    subprocess.check_call(cmd.format(**locals()), shell=True)
+    return out_file
+
+
+def genepred_to_UCSCtable(genepred, out_file):
+    # header = ["#bin", "name", "chrom", "strand",
+    #           "txStart", "txEnd", "cdsStart", "cdsEnd",
+    #           "exonCount", "exonStarts", "exonEnds", "score",
+    #           "name2", "cdsStartStat", "cdsEndStat",
+    #           "exonFrames"]
+    if file_exists(out_file):
+        logger.warn("Skipping genepred_to_UCSC_table as files already exist!")
+        return out_file
+    with open(genepred) as in_handle, gzip.open(out_file, "wb") as out_handle:
+        counter = -1
+        current_item = None
+        # out_handle.write("\t".join(header) + "\n")
+        for l in in_handle:
+            item = l.split("\t")[0]
+            if current_item != item:
+                current_item = item
+                counter = counter + 1
+            out_handle.write("\t".join([str(counter), l]))
+    return out_file
