e41b05eb608c65b3fbb902eaa93b4fe09741e240
Jeff Jasper <jasper1918@gmail.com>
2016-12-23 16:08:33 -0500

    2and3 compatible

--- setup.py
+++ setup.py
@@ -2,28 +2,31 @@
 
 from setuptools import setup, find_packages, Extension
 import sys
-import os
+from io import open
 from sys import stderr
-from ez_setup import use_setuptools
-use_setuptools()
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def get_version(string):
+    """ Parse the version number variable __version__ from a script. """
+    import re
+    version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    version_str = re.search(version_re, string, re.M).group(1)
+    return version_str
 
-libssw_ext = Extension('_libssw_ext', sources = ['ssw/src/ssw.c'], include_dirs = ['ssw/src/'])
+
+libssw_ext = Extension('_libssw_ext', sources=['ssw/src/ssw.c'], include_dirs=['ssw/src/'])
 
 setup(
     name='STAR-SEQR',
-    version='0.1.0',
-    description='',
-    long_description=read('README.rst'),
+    version=get_version(open('starseqr_utils/__init__.py', encoding='utf-8').read()),
+    description='RNA-Fusion Caller',
+    long_description=open('README.rst', encoding='utf-8').read(),
     license='Custom',
     author='Jeff Jasper',
     author_email='jasper1918@gmail.com',
     url='https://github.com/ExpressionAnalysis/STAR-SEQR',
     packages=find_packages(),
-    install_requires=['six', 'pandas>=0.18.0', 'pysam==0.9.0', 'primer3-py', 'intervaltree_bio'],
+    install_requires=['six', 'pandas>=0.18.0', 'pysam>=0.9.0', 'primer3-py', 'intervaltree_bio'],
     ext_modules=[libssw_ext],
     package_data={"starseqr_utils": ["resources/*"]},
     scripts=['starseqr.py'],
