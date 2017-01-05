935fa1eccdef2300bedb55c5340e26fccf731493
Jeff Jasper <jasper1918@gmail.com>
2017-01-05 17:12:57 -0500

    new docker repo

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,11 +2,11 @@
 
 VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
 
-cmd1='docker build -t eagenomics/star-seqr:${VERSION} --rm --file Dockerfile .'
+cmd1='docker build -t eagenomics/starseqr:${VERSION} --rm --file Dockerfile .'
 echo $cmd1
 eval $cmd1
 
-cmd2='docker build -t eagenomics/star-seqr:latest --rm --file Dockerfile .'
+cmd2='docker build -t eagenomics/starseqr:latest --rm --file Dockerfile .'
 echo $cmd2
 eval $cmd2
 
--- docker/push_docker.sh
+++ docker/push_docker.sh
@@ -4,10 +4,10 @@ docker login --username=jasper1918
 
 VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
 
-cmd1="docker push eagenomics/star-seqr:${VERSION}"
+cmd1="docker push eagenomics/starseqr:${VERSION}"
 echo $cmd1
 eval $cmd1
 
-cmd2="docker push eagenomics/star-seqr:latest"
+cmd2="docker push eagenomics/starseqr:latest"
 echo $cmd2
 eval $cmd2
