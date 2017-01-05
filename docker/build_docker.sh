996afbd1508472cb674af93506c4fe0bfbc3d340
jasper1918 <jasper1918@gmail.com>
2017-01-05 18:31:44 -0500

    better builds

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+## This file is intended to be run using sudo
+
 VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
 
-cmd1='docker build -t eagenomics/starseqr:${VERSION} --rm --file Dockerfile .'
+cmd0='sudo service docker start'
+echo $cmd0
+eval $cmd0
+
+cmd1='sudo docker build --rm --no-cache -t eagenomics/starseqr:${VERSION} -t eagenomics/starseqr:latest --file Dockerfile .'
 echo $cmd1
 eval $cmd1
 
-cmd2='docker build -t eagenomics/starseqr:latest --rm --file Dockerfile .'
-echo $cmd2
-eval $cmd2
 
