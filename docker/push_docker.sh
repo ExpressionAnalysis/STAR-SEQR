4a76db7f2e36b91d5a9860961919135e5f0be7c4
Jeff Jasper <jasper1918@gmail.com>
2017-01-05 17:08:04 -0500

    new docker files

---
+++ docker/push_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker login --username=jasper1918
+
+VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
+
+cmd1="docker push eagenomics/star-seqr:${VERSION}"
+echo $cmd1
+eval $cmd1
+
+cmd2="docker push eagenomics/star-seqr:latest"
+echo $cmd2
+eval $cmd2
