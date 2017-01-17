c3247ab2fee7848a2512770a5326c22e997838eb
Jeff Jasper <jasper1918@gmail.com>
2017-01-17 13:25:54 -0500

    version not needed

--- docker/cleanup_docker.sh
+++ docker/cleanup_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # this file is intended to be run using sudo
 
-VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
-
 cmd0='service docker start'
 echo $cmd0
 eval $cmd0
