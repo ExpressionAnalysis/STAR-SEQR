460abc3c6bd99b6e5fbf847a2c948f36e68c5b3d
Jeff Jasper <jasper1918@gmail.com>
2017-02-21 17:01:22 -0500

    change file names

--- starseqr_utils/cross_homology.py
+++ starseqr_utils/cross_homology.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from __future__ import (absolute_import, division, print_function)
+import os
 import logging
 import ssw
 import numpy as np
@@ -15,7 +16,7 @@ def get_ssw_scores(reads_fq, trxleft_fa, trxright_fa, mm_rate=2, mm_pct=.05):
     aligner = ssw.Aligner(gap_open=12, gap_extend=4)
     rfq_gen = su.common.FastqParser(reads_fq)
     matches = []
-    for rfq in rfq_gen:
+    for rfq in (x for _, x in zip(range(500), rfq_gen)):  # just do top 500 reads to maintain speed
         l_max = 0
         r_max = 0
         mod_len = min(rfq.seq_len - mm_rate, rfq.seq_len - rfq.seq_len * mm_pct)  # control error rates across a diversity of read len
@@ -30,10 +31,7 @@ def get_ssw_scores(reads_fq, trxleft_fa, trxright_fa, mm_rate=2, mm_pct=.05):
         read_norm = min(l_max, r_max) / mod_len  # pct identity matching
         matches.append(read_norm / 2)
     if len(matches) > 0:
-        # return (matches)
         return(float("{0:.3f}".format(np.sum(i > .8 for i in matches) / len(matches))))
-        # return(float("{0:.3f}".format(np.sum(i > 32 for i in matches) / len(matches)))) # switch to proportion of reads > 32 (16bp homology) #TODO profile
-        # return(int(np.percentile(matches, 75)))  # UQ75 of ssw from cross-mapping
     else:
         return(0)
 
@@ -42,12 +40,11 @@ def get_cross_homology(jxn):
     paired_res = None
     overhang_res = None
     clean_jxn = su.common.safe_jxn(jxn)
-    jxn_dir = 'support' + '/' + clean_jxn + '/'
-
-    trans_left_fa = jxn_dir + 'transcripts_left.fa'
-    trans_right_fa = jxn_dir + 'transcripts_right.fa'
-    paired_fq = jxn_dir + 'span.fastq'  # this file contains overhang/junction reads.
-    overhang_fq = jxn_dir + 'overhang.fastq'
+    jxn_dir = os.path.join('support', clean_jxn)
+    trans_left_fa = os.path.join(jxn_dir, 'transcripts_left.fa')
+    trans_right_fa = os.path.join(jxn_dir, 'transcripts_right.fa')
+    paired_fq = os.path.join(jxn_dir, 'span.fastq')
+    overhang_fq = os.path.join(jxn_dir, 'overhang.fastq')
     paired_res = get_ssw_scores(paired_fq, trans_left_fa, trans_right_fa)
     overhang_res = get_ssw_scores(overhang_fq, trans_left_fa, trans_right_fa)
     return paired_res, overhang_res
