16361841bb99f374f2604905336a2285d3d28431
Jeff Jasper <jasper1918@gmail.com>
2017-12-07 17:47:37 -0500

    relative path changed

--- devtools/docker/push_docker.sh
+++ devtools/docker/push_docker.sh
@@ -2,7 +2,7 @@
 
 docker login --username=jasper1918
 
-VERSION=$(grep version ../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
+VERSION=$(grep version ../../starseqr_utils/__init__.py | cut -d "=" -f2 | tr -d " \t\n\r\"\'"s)
 
 cmd1="docker push eagenomics/starseqr:${VERSION}"
 echo $cmd1
