8889bc879c647c2174c37ee4a69a27626814c22b
Jeff Jasper <jasper1918@gmail.com>
2018-04-06 14:27:00 -0400

    again

--- starseqr_utils/tests/test_primer.py
+++ starseqr_utils/tests/test_primer.py
@@ -17,16 +17,16 @@ class PrimerTestCase(unittest.TestCase):
         seq = "CATCAGCAGAATTCCCGCCATAGGTTAACTCTTTGACATTCTTATTACCACTGTGGGTCTCTTTGGGATCCCAGGGCACTGTAGGCAACTAGAACAATGTCCTTGGACTTGCAGAACTCCAGGAGTTTGCTCTGGTTGAGGTAAGGGTGACATTCCACCTGTAA:TTTTTCAAGACCAAAAGGTGTCACAATATATGAGCCAATAACCACATGCAAATTTAAACTGGAAAATAGAAAAGAAGATAAAGAAGGACATTTTCTATGGAATTCTCCTTTCTGGTGCCTAAGGAGCTCGGCCAAACAACCTTCAAGAGTTTCAGGAGGTGGTGATTAT"
         primer_res = su.run_primer3.runp3('test1', seq)
         assert(len(primer_res) == 2)
-        assert(len(primer_res[0] > 0))
-        assert(len(primer_res[1] > 0))
+        assert(len(primer_res[0]) > 0)
+        assert(len(primer_res[1]) > 0)
 
     def test_runp3_v1_1(self):
         """same as v1 in middle but this time use the location"""
         seq = "CATCAGCAGAATTCCCGCCATAGGTTAACTCTTTGACATTCTTATTACCACTGTGGGTCTCTTTGGGATCCCAGGGCACTGTAGGCAACTAGAACAATGTCCTTGGACTTGCAGAACTCCAGGAGTTTGCTCTGGTTGAGGTAAGGGTGACATTCCACCTGTAATTTTTCAAGACCAAAAGGTGTCACAATATATGAGCCAATAACCACATGCAAATTTAAACTGGAAAATAGAAAAGAAGATAAAGAAGGACATTTTCTATGGAATTCTCCTTTCTGGTGCCTAAGGAGCTCGGCCAAACAACCTTCAAGAGTTTCAGGAGGTGGTGATTAT"
         primer_res = su.run_primer3.runp3('test1', seq, 164)
         assert(len(primer_res) == 2)
-        assert(len(primer_res[0] > 0))
-        assert(len(primer_res[1] > 0))
+        assert(len(primer_res[0]) > 0)
+        assert(len(primer_res[1]) > 0)
 
     def test_runp3_v2(self):
         """test_runp3 with no target"""
@@ -51,16 +51,16 @@ class PrimerTestCase(unittest.TestCase):
         seq = "CATCAGCAGAATTCCCGCCATAGGTTAACTCTTTGACATTCTTATTACCACTGTGGGTCTCTTTGGGATCCCAGGGCACTGTAGGCAACTAGAACAATGTCCTTGGACTTGCAGAACTCCAGGAGTTTGCTCTGGTTGAGGTAAGGGTGACATTCCACCTGTAATTTTTCAAGACCAAAAGGTGTCACAATATATGAGCCAATAACCACATGCAAATTTAAACTGGAAAATAGAAAAGAAGATAAAGAAGGACATTTTCTATGGAATTCTCCTTTCTGGTGCCTAAGGAGCTCGGCCAAACAACCTTCAAGAGTTTCAGGAGGTGGTGATTAT"
         primer_res = su.run_primer3.runp3('test1', seq)
         assert(len(primer_res) == 2)
-        assert(len(primer_res[0] > 0))
-        assert(len(primer_res[1] > 0))
+        assert(len(primer_res[0]) > 0)
+        assert(len(primer_res[1]) > 0)
 
     def test_wrap_runp3_v1(self):
         """test wrapper for runp3"""
-        fusions = 'ENST00000372201.4_1--ENST00000510927.5_1'
+        fusions = 'ENST00000372201.4_1--ENST00000510927.5_1|988'
         res1 = su.run_primer3.wrap_runp3('chr1:45268528:+:chr4:107152937:-:0:2', fusions, "test_data/chim_transcripts/")
         assert(len(res1) == 2)
-        assert(len(res1[0] > 0))
-        assert(len(res1[1] > 0))
+        assert(len(res1[0]) > 0)
+        assert(len(res1[1]) > 0)
 
     def test_wrap_runp3_v2(self):
         """test wrapper for runp3. Empty fusion list"""
